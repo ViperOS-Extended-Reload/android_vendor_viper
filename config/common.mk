5cba22647620ecd6c00c26ab7490af514fa468ec
Thespartann <danilastefan69@yahoo.ro>
2019-04-12 06:53:26 -0400

    vendor: Bump version to 3.1.3
    * Updoot Rom to 2019 April Security patch
    * Update translations
    * Misc stuff
    
    Note: This is the final update provided by me to this fork, goodbye :)
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- config/common.mk
+++ config/common.mk
@@ -264,7 +264,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/viper/overlay/common
 # Versioning System
 # ViperOs version.
 VIPER_VERSION_CODENAME := Extended
-VIPER_VERSION_NUMBER := v3.1.2
+VIPER_VERSION_NUMBER := v3.1.3
 
 VIPER_DEVICE := $(VIPER_BUILD)
 
